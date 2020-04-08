a1ab956f24f0bdeca6b7c52cbb63f1216f3416a8
alimemco <alimoghadam75@gmail.com>
2020-04-08 14:56:25 +0430

    rotate and change color of add button

--- app/src/main/java/com/alirnp/curvedbottomnavigationview/MainActivity.java
+++ app/src/main/java/com/alirnp/curvedbottomnavigationview/MainActivity.java
@@ -1,25 +1,68 @@
 package com.alirnp.curvedbottomnavigationview;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.res.ColorStateList;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.OvershootInterpolator;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.content.ContextCompat;
+import androidx.core.view.ViewCompat;
+import androidx.core.widget.ImageViewCompat;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private float original = 0f;
+    private float rotated = 45f;
+    private boolean isOriginalMode = true;
+
+    private ImageView mImageView;
+    private ConstraintLayout mConstraintLayout;
+    private ConstraintLayout mConstraintLayoutRoot;
+
+    private int mColorPrimary;
+    private int mColorWhite;
+
+    public static void rotateOvershoot(View view, float rotate) {
+        OvershootInterpolator interpolator = new OvershootInterpolator();
+
+        ViewCompat.animate(view).
+                rotation(rotate).
+                withLayer().
+                setDuration(400).
+                setInterpolator(interpolator).
+                start();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        CurvedBottomNavigationView mView = findViewById(R.id.customBottomBar);
-      //  mView.inflateMenu(R.menu.bottom_menu);
-        mView.setSelectedItemId(R.id.action_schedules);
-        mView.getMenu().findItem(R.id.action_schedules).setEnabled(false);
 
-        //getting bottom navigation view and attaching the listener
+        CurvedBottomNavigationView mView = findViewById(R.id.customBottomBar);
+        mImageView = findViewById(R.id.fab);
+        mConstraintLayout = findViewById(R.id.lin_id);
+        mConstraintLayoutRoot = findViewById(R.id.root);
+
+        mColorPrimary = ContextCompat.getColor(MainActivity.this, R.color.colorPrimary);
+        mColorWhite = ContextCompat.getColor(MainActivity.this, R.color.white);
+
+        mView.setSelectedItemId(R.id.action_favorites);
+        mView.getMenu().findItem(R.id.action_schedules).setCheckable(false);
+/*
+        View beneathView = //the view that beneath blur view
+                View blurView= //blur View
+*/
+
+
+
+
         mView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -27,7 +70,8 @@ public class MainActivity extends AppCompatActivity {
                     case R.id.action_favorites:
                         break;
                     case R.id.action_schedules:
-                        return false;
+                        validateState();
+                        break;
                     case R.id.action_music:
                         break;
                 }
@@ -35,6 +79,32 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+
+        mImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                validateState();
+            }
+        });
+
+        mConstraintLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                validateState();
+            }
+        });
+
+
     }
 
+    private void validateState() {
+
+        isOriginalMode = !isOriginalMode;
+
+        rotateOvershoot(mImageView, isOriginalMode ? original : rotated);
+        ImageViewCompat.setImageTintList(mImageView, ColorStateList.valueOf(isOriginalMode ? mColorPrimary : mColorWhite));
+        mConstraintLayout.setBackground(isOriginalMode ? ContextCompat.getDrawable(MainActivity.this, R.drawable.shape_circle) : ContextCompat.getDrawable(MainActivity.this, R.drawable.shape_circle_opend));
+
+    }
 }
